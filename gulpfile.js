790ee5b4a18106a8ab774f8a139779067ce696e2
peterlaxalt <pete@laxaltandmciver.co>
2018-10-18 11:19:21 -0400

    gulp inline css workflow + live file watching + live file inlining

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,42 @@
-var gulp = require('gulp'),
-    inlineCss = require('gulp-inline-css');
+var gulp = require("gulp");
+var inlineCss = require("gulp-inline-css");
+var run = require("gulp-run");
+var notify = require("gulp-notify");
+var browserSync = require("browser-sync").create();
 
-gulp.task('inline', function () {
-    return gulp.src('./_site/*.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('build/'));
+// inliner task
+gulp.task("inline", function() {
+  return gulp
+    .src("./src/_site/*.html")
+    .pipe(inlineCss())
+    .pipe(gulp.dest("build/"));
 });
 
-gulp.task("watch", function () {
-    return gulp.watch(['./_site/*.html'], ['inline']);;
+// watch for new generated jekyll files and inline them
+gulp.task("watch", function() {
+  return gulp.watch(["./src/_site/*.html"], ["inline"]);
+});
+
+// serve & compile jekyll
+gulp.task("serve", function() {
+  var shellCommand = "cd src && bundle exec jekyll serve";
+  return gulp
+    .src("")
+    .pipe(run(shellCommand))
+    .pipe(notify({ message: "🍕 uninlined server running at localhost:4000" }));
+});
+
+// use default task to launch Browsersync and watch JS files
+gulp.task("default", ["serve", "watch"], function() {
+  // serve files from inlined src folder
+  browserSync.init({
+    server: {
+      baseDir: "./build/",
+      port: 8080
+    }
+  });
+  // add browserSync.reload to the tasks array to make
+  // all browsers reload after tasks are complete.
+  gulp.watch("./build/*.html", browserSync.reload());
+  gulp.pipe(notify({ message: "✅ inlined server running at localhost:8080" }));
 });
--- readme.md
+++ readme.md
@@ -1,13 +1,20 @@
 # Covered Hubspot Email Builder
 Made in Jekyll
 
-## To Run
+# To Start
 `bundle install`
+`npm install`
 
-`bundle exec jekyll serve`
+## To Run
+Simply run `gulp` from the root directory.
+
+## To Debug
+1. `cd src && bundle exec jekyll serve` from root.
+2. `gulp watch` from root.
+3. View your files from the `/build/` folder.
 
 ## Fire up an .ngrok server to test on your devices
-  - You can access your ```localhost:4000``` from any device because ```.ngrok``` is amazing.
+  - You can access your ```localhost``` from any device because ```.ngrok``` is amazing.
   - In the ```/_ngrok/``` folder you'll find a .zip file with .ngrok in it. Install that.
-  - In terminal, simply type ```$ ./ngrok http 4000``` to fire up a server and they will provide urls.
+  - In terminal, simply type ```$ ./ngrok http ####``` to fire up a server and they will provide urls. `####` will be your desired port number.
   - Be sure that if you are on development and potentially behind a directory such as ```/covered-hubspot/```, you'll have to append that to your .ngrok url.
